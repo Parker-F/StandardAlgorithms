2d11f6e09651a64ff21ece3fab587d89a9f926f2
Fred Parker <parker.f@etoncollege.org.uk>
2023-11-21 10:38:21 +0000

    linearSearch uncompleted

--- StandardAlgorithms/Searching.swift
+++ StandardAlgorithms/Searching.swift
@@ -9,10 +9,10 @@ import Foundation
 
 class Searching {
     func linearSearch(_ numbers: [Int],_ key: Int) -> Int {
-        
         for i in 0...numbers.count {
-            if key == numbers[i-1] {
-                return numbers[i-1]
+            var number = numbers[i-1]
+            if key == number {
+                return number
             }
             
         }
